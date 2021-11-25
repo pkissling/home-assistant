3c47ab3e6b518d2251e867191fac9d6fc50e4f6a
Patrick Kissling <patrick.kissling@gmx.de>
2021-11-25 21:30:51 +0100

    Fix bug where light would be considered off immediately

--- pyscript/lights.py
+++ pyscript/lights.py
@@ -1,7 +1,7 @@
 @service
 def light_ambient_turn_on(
-  light_entity_id=None, 
-  motion_sensor_entity_id=None, 
+  light_entity_id=None,
+  motion_sensor_entity_id=None,
   full_brightness=200,
   dim_brightness=50,
   dim_after_mins=2):
@@ -9,11 +9,11 @@ def light_ambient_turn_on(
     task.unique(light_entity_id)
 
     # conditions
-    if not meets_conditions():
+    if is_sun():
       return
 
     log.info(f"START light_ambient_turn_on: light_entity_id={light_entity_id}")
-   
+
     # action
     if needs_full_brightness(light_entity_id):
         log.info(f"Turn {light_entity_id} to full brightness")
@@ -23,7 +23,7 @@ def light_ambient_turn_on(
 
         # wait until motion sensor is off
         task.wait_until(state_trigger=f"{motion_sensor_entity_id} == 'off'", timeout=60 * 60)
-        
+
         # sleep for x mins before turning off lights
         task.sleep(dim_after_mins * 60)
 
@@ -32,6 +32,9 @@ def light_ambient_turn_on(
             light.turn_on(entity_id=light_entity_id, transition=60, brightness=dim_brightness)
 
     else:
+
+      if is_vacuum_running():
+          return
 
       log.info(f"Turn {light_entity_id} to low brightness")
 
@@ -50,22 +53,24 @@ def light_ambient_turn_on(
       # turn off lights
       light.turn_off(entity_id=light_entity_id, transition=60)
 
-      # wait max 10 mins until lights off to turn off night light
-      task.wait_until(state_trigger=f"{light_entity_id} == 'off'", timeout=60 * 60)
+      # turn off night light
+      task.sleep(60)
       input_boolean.turn_off(entity_id='input_boolean.night_light')
 
     log.info(f"END light_ambient_turn_on: light_entity_id={light_entity_id}")
 
 
-def meets_conditions():
-  return (vacuum.xiaomi_vacuum_cleaner != 'cleaning'
-    and vacuum.xiaomi_vacuum_cleaner != 'returning'
-    and binary_sensor.sun == 'off')
+def is_sun():
+  return binary_sensor.sun == 'on'
 
 def needs_full_brightness(light_entity_id):
   return (is_light_on(light_entity_id)
-      and input_boolean.night_light == 'off')
+    and input_boolean.night_light == 'off')
 
 def is_light_on(light_entity_id):
   return (state.get(light_entity_id) == 'on'
-      and state.get('script.lights_ambient_turn_off') == 'off')
+    and state.get('script.lights_ambient_turn_off') == 'off')
+
+def is_vacuum_running():
+   return (vacuum.xiaomi_vacuum_cleaner == 'cleaning'
+    or vacuum.xiaomi_vacuum_cleaner == 'returning')

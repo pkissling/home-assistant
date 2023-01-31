a1fed4b74d711c1ad7a65b098741701f5f2f581a
Patrick Kissling <patrick.kissling@gmx.de>
2023-01-31 17:13:20 +0100

    Create stock dashboard

---
+++ custom_components/yahoofinance/__init__.py
@@ -0,0 +1,265 @@
+"""
+The Yahoo finance component.
+
+https://github.com/iprak/yahoofinance
+"""
+
+from __future__ import annotations
+
+from datetime import timedelta
+import logging
+
+from homeassistant.const import CONF_SCAN_INTERVAL
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import discovery
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.typing import ConfigType
+import voluptuous as vol
+
+from custom_components.yahoofinance.coordinator import YahooSymbolUpdateCoordinator
+
+from .const import (
+    CONF_DECIMAL_PLACES,
+    CONF_INCLUDE_FIFTY_DAY_VALUES,
+    CONF_INCLUDE_POST_VALUES,
+    CONF_INCLUDE_PRE_VALUES,
+    CONF_INCLUDE_TWO_HUNDRED_DAY_VALUES,
+    CONF_SHOW_TRENDING_ICON,
+    CONF_SYMBOLS,
+    CONF_TARGET_CURRENCY,
+    DEFAULT_CONF_DECIMAL_PLACES,
+    DEFAULT_CONF_INCLUDE_FIFTY_DAY_VALUES,
+    DEFAULT_CONF_INCLUDE_POST_VALUES,
+    DEFAULT_CONF_INCLUDE_PRE_VALUES,
+    DEFAULT_CONF_INCLUDE_TWO_HUNDRED_DAY_VALUES,
+    DEFAULT_CONF_SHOW_TRENDING_ICON,
+    DEFAULT_SCAN_INTERVAL,
+    DOMAIN,
+    HASS_DATA_CONFIG,
+    HASS_DATA_COORDINATORS,
+    MINIMUM_SCAN_INTERVAL,
+    SERVICE_REFRESH,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+BASIC_SYMBOL_SCHEMA = vol.All(cv.string, vol.Upper)
+
+COMPLEX_SYMBOL_SCHEMA = vol.All(
+    dict,
+    vol.Schema(
+        {
+            vol.Required("symbol"): BASIC_SYMBOL_SCHEMA,
+            vol.Optional(CONF_TARGET_CURRENCY): BASIC_SYMBOL_SCHEMA,
+            vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): vol.Any(
+                "none", "None", cv.positive_time_period
+            ),
+        }
+    ),
+)
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_SYMBOLS): vol.All(
+                    cv.ensure_list,
+                    [vol.Any(BASIC_SYMBOL_SCHEMA, COMPLEX_SYMBOL_SCHEMA)],
+                ),
+                vol.Optional(
+                    CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL
+                ): vol.Any("none", "None", cv.positive_time_period),
+                vol.Optional(CONF_TARGET_CURRENCY): vol.All(cv.string, vol.Upper),
+                vol.Optional(
+                    CONF_SHOW_TRENDING_ICON, default=DEFAULT_CONF_SHOW_TRENDING_ICON
+                ): cv.boolean,
+                vol.Optional(
+                    CONF_DECIMAL_PLACES, default=DEFAULT_CONF_DECIMAL_PLACES
+                ): vol.Coerce(int),
+                vol.Optional(
+                    CONF_INCLUDE_FIFTY_DAY_VALUES,
+                    default=DEFAULT_CONF_INCLUDE_FIFTY_DAY_VALUES,
+                ): cv.boolean,
+                vol.Optional(
+                    CONF_INCLUDE_POST_VALUES, default=DEFAULT_CONF_INCLUDE_POST_VALUES
+                ): cv.boolean,
+                vol.Optional(
+                    CONF_INCLUDE_PRE_VALUES, default=DEFAULT_CONF_INCLUDE_PRE_VALUES
+                ): cv.boolean,
+                vol.Optional(
+                    CONF_INCLUDE_TWO_HUNDRED_DAY_VALUES,
+                    default=DEFAULT_CONF_INCLUDE_TWO_HUNDRED_DAY_VALUES,
+                ): cv.boolean,
+            }
+        )
+    },
+    # The complete HA configuration is passed down to`async_setup`, allow the extra keys.
+    extra=vol.ALLOW_EXTRA,
+)
+
+
+class SymbolDefinition:
+    """Symbol definition."""
+
+    symbol: str
+    target_currency: str | None = None
+    scan_interval: timedelta | None = None
+
+    def __init__(self, symbol: str, **kwargs: any) -> None:
+        """Create a new symbol definition.
+
+        ### Parameters
+            symbol(str): The symbol
+            **scan_interval (time_delta): The symbol scan interval
+        """
+        self.symbol = symbol
+
+        if "target_currency" in kwargs:
+            self.target_currency = kwargs["target_currency"]
+        if "scan_interval" in kwargs:
+            self.scan_interval = kwargs["scan_interval"]
+
+    def __repr__(self) -> str:
+        """Return the representation."""
+        return f"{self.symbol},{self.target_currency},{self.scan_interval}"
+
+    def __eq__(self, other: any) -> bool:
+        """Return the comparison."""
+        return (
+            isinstance(other, SymbolDefinition)
+            and self.symbol == other.symbol
+            and self.target_currency == other.target_currency
+            and self.scan_interval == other.scan_interval
+        )
+
+    def __hash__(self) -> int:
+        """Make hashable."""
+        return hash((self.symbol, self.target_currency, self.scan_interval))
+
+
+def parse_scan_interval(scan_interval: timedelta | str) -> timedelta:
+    """Parse and validate scan_interval."""
+    if isinstance(scan_interval, str):
+        if isinstance(scan_interval, str):
+            if scan_interval.lower() == "none":
+                scan_interval = None
+            else:
+                raise vol.Invalid(
+                    f"Invalid {CONF_SCAN_INTERVAL} specified: {scan_interval}"
+                )
+    elif scan_interval < MINIMUM_SCAN_INTERVAL:
+        raise vol.Invalid("Scan interval should be at least 30 seconds.")
+
+    return scan_interval
+
+
+def normalize_input_symbols(
+    defined_symbols: list,
+) -> tuple[list[str], list[SymbolDefinition]]:
+    """Normalize input and remove duplicates."""
+    symbols = set()
+    symbol_definitions: list[SymbolDefinition] = []
+
+    for value in defined_symbols:
+        if isinstance(value, str):
+            if value not in symbols:
+                symbols.add(value)
+                symbol_definitions.append(SymbolDefinition(value))
+        else:
+            symbol = value["symbol"]
+            if symbol not in symbols:
+                symbols.add(symbol)
+                symbol_definitions.append(
+                    SymbolDefinition(
+                        symbol,
+                        target_currency=value.get(CONF_TARGET_CURRENCY),
+                        scan_interval=parse_scan_interval(
+                            value.get(CONF_SCAN_INTERVAL)
+                        ),
+                    )
+                )
+
+    return (list(symbols), symbol_definitions)
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the component."""
+    domain_config = config.get(DOMAIN, {})
+    defined_symbols = domain_config.get(CONF_SYMBOLS, [])
+
+    symbol_definitions: list[SymbolDefinition]
+    symbols, symbol_definitions = normalize_input_symbols(defined_symbols)
+    domain_config[CONF_SYMBOLS] = symbol_definitions
+
+    scan_interval = parse_scan_interval(domain_config.get(CONF_SCAN_INTERVAL))
+
+    # Populate parsed value into domain_config
+    domain_config[CONF_SCAN_INTERVAL] = scan_interval
+
+    # Group symbols by scan_interval
+    symbols_by_scan_interval: dict[timedelta, list[str]] = {}
+    for symbol in symbol_definitions:
+        # Use integration level scan_interval if none defined
+        if symbol.scan_interval is None:
+            symbol.scan_interval = scan_interval
+
+        if symbol.scan_interval in symbols_by_scan_interval:
+            symbols_by_scan_interval[symbol.scan_interval].append(symbol.symbol)
+        else:
+            symbols_by_scan_interval[symbol.scan_interval] = [symbol.symbol]
+
+    _LOGGER.info("Total %d unique scan intervals", len(symbols_by_scan_interval))
+
+    coordinators: dict[timedelta, YahooSymbolUpdateCoordinator] = {}
+    for key_scan_interval, symbols in symbols_by_scan_interval.items():
+        _LOGGER.info(
+            "Creating coordinator with scan_interval %s for symbols %s",
+            key_scan_interval,
+            symbols,
+        )
+        coordinator = YahooSymbolUpdateCoordinator(symbols, hass, key_scan_interval)
+        coordinators[key_scan_interval] = coordinator
+
+        _LOGGER.info(
+            "Requesting initial data from coordinator with update interval of %s.",
+            key_scan_interval,
+        )
+        await coordinator.async_refresh()
+
+    # Pass down the coordinator and config to platforms.
+    hass.data[DOMAIN] = {
+        HASS_DATA_COORDINATORS: coordinators,
+        HASS_DATA_CONFIG: domain_config,
+    }
+
+    async def handle_refresh_symbols(_call) -> None:
+        """Refresh symbol data."""
+        _LOGGER.info("Processing refresh_symbols")
+
+        for coordinator in coordinators.values():
+            await coordinator.async_refresh()
+
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_REFRESH,
+        handle_refresh_symbols,
+    )
+
+    if not coordinator.last_update_success:
+        _LOGGER.debug("Coordinator did not report any data, requesting async_refresh")
+        hass.async_create_task(coordinator.async_request_refresh())
+
+    hass.async_create_task(
+        discovery.async_load_platform(hass, "sensor", DOMAIN, {}, config)
+    )
+
+    return True
+
+
+def convert_to_float(value) -> float | None:
+    """Convert specified value to float."""
+    try:
+        return float(value)
+    except:  # noqa: E722 pylint: disable=bare-except
+        return None
---
+++ custom_components/yahoofinance/const.py
@@ -0,0 +1,145 @@
+"""Constants for Yahoo Finance sensor."""
+
+from __future__ import annotations
+
+from datetime import timedelta
+from typing import Final
+
+# Additional attributes exposed by the sensor
+ATTR_CURRENCY_SYMBOL: Final = "currencySymbol"
+ATTR_QUOTE_TYPE: Final = "quoteType"
+ATTR_QUOTE_SOURCE_NAME: Final = "quoteSourceName"
+ATTR_SYMBOL: Final = "symbol"
+ATTR_TRENDING: Final = "trending"
+ATTR_MARKET_STATE: Final = "marketState"
+ATTR_DIVIDEND_DATE: Final = "dividendDate"
+
+# Hass data
+HASS_DATA_CONFIG: Final = "config"
+HASS_DATA_COORDINATORS: Final = "coordinators"
+
+# JSON data pieces
+DATA_CURRENCY_SYMBOL: Final = "currency"
+DATA_FINANCIAL_CURRENCY: Final = "financialCurrency"
+DATA_QUOTE_TYPE: Final = "quoteType"
+DATA_QUOTE_SOURCE_NAME: Final = "quoteSourceName"
+DATA_SHORT_NAME: Final = "shortName"
+DATA_MARKET_STATE: Final = "marketState"
+DATA_DIVIDEND_DATE: Final = "dividendDate"
+
+DATA_REGULAR_MARKET_PREVIOUS_CLOSE: Final = "regularMarketPreviousClose"
+DATA_REGULAR_MARKET_PRICE: Final = "regularMarketPrice"
+
+CONF_DECIMAL_PLACES: Final = "decimal_places"
+CONF_INCLUDE_FIFTY_DAY_VALUES: Final = "include_fifty_day_values"
+CONF_INCLUDE_POST_VALUES: Final = "include_post_values"
+CONF_INCLUDE_PRE_VALUES: Final = "include_pre_values"
+CONF_INCLUDE_TWO_HUNDRED_DAY_VALUES: Final = "include_two_hundred_day_values"
+CONF_SHOW_TRENDING_ICON: Final = "show_trending_icon"
+CONF_TARGET_CURRENCY: Final = "target_currency"
+
+DEFAULT_CONF_DECIMAL_PLACES: Final = 2
+DEFAULT_CONF_INCLUDE_FIFTY_DAY_VALUES: Final = True
+DEFAULT_CONF_INCLUDE_POST_VALUES: Final = True
+DEFAULT_CONF_INCLUDE_PRE_VALUES: Final = True
+DEFAULT_CONF_INCLUDE_TWO_HUNDRED_DAY_VALUES: Final = True
+DEFAULT_CONF_SHOW_TRENDING_ICON: Final = False
+
+DEFAULT_NUMERIC_DATA_GROUP: Final = "default"
+
+# Data keys grouped into categories. The values for the categories except for
+# DEFAULT_NUMERIC_DATA_GROUP can be conditionally pulled into attributes. The
+# first value of the set is the key and the second boolean value indicates if
+# the attribute is a currency.
+NUMERIC_DATA_GROUPS: Final = {
+    DEFAULT_NUMERIC_DATA_GROUP: [
+        ("averageDailyVolume10Day", False),
+        ("averageDailyVolume3Month", False),
+        ("regularMarketChange", True),
+        ("regularMarketChangePercent", False),
+        ("regularMarketDayHigh", True),
+        ("regularMarketDayLow", True),
+        (DATA_REGULAR_MARKET_PREVIOUS_CLOSE, True),
+        (DATA_REGULAR_MARKET_PRICE, True),
+        ("regularMarketVolume", False),
+        ("regularMarketTime", False),
+        (DATA_DIVIDEND_DATE, False),
+    ],
+    CONF_INCLUDE_FIFTY_DAY_VALUES: [
+        ("fiftyDayAverage", True),
+        ("fiftyDayAverageChange", True),
+        ("fiftyDayAverageChangePercent", False),
+    ],
+    CONF_INCLUDE_PRE_VALUES: [
+        ("preMarketChange", True),
+        ("preMarketChangePercent", False),
+        ("preMarketTime", False),
+        ("preMarketPrice", True),
+    ],
+    CONF_INCLUDE_POST_VALUES: [
+        ("postMarketChange", True),
+        ("postMarketChangePercent", False),
+        ("postMarketPrice", True),
+        ("postMarketTime", False),
+    ],
+    CONF_INCLUDE_TWO_HUNDRED_DAY_VALUES: [
+        ("twoHundredDayAverage", True),
+        ("twoHundredDayAverageChange", True),
+        ("twoHundredDayAverageChangePercent", False),
+    ],
+}
+
+PERCENTAGE_DATA_KEYS_NEEDING_MULTIPLICATION: Final = [
+    "fiftyDayAverageChangePercent",
+    "twoHundredDayAverageChangePercent",
+]
+
+
+# Defaults for missing numeric keys
+NUMERIC_DATA_DEFAULTS: Final = {DATA_DIVIDEND_DATE: None}
+
+STRING_DATA_KEYS: Final = [
+    DATA_CURRENCY_SYMBOL,
+    DATA_FINANCIAL_CURRENCY,
+    DATA_QUOTE_TYPE,
+    DATA_QUOTE_SOURCE_NAME,
+    DATA_SHORT_NAME,
+    DATA_MARKET_STATE,
+]
+
+
+ATTRIBUTION: Final = "Data provided by Yahoo Finance"
+BASE: Final = "https://query1.finance.yahoo.com/v7/finance/quote?symbols="
+
+CONF_SYMBOLS: Final = "symbols"
+DEFAULT_CURRENCY: Final = "USD"
+DEFAULT_CURRENCY_SYMBOL: Final = "$"
+DEFAULT_ICON: Final = "mdi:currency-usd"
+DOMAIN: Final = "yahoofinance"
+SERVICE_REFRESH: Final = "refresh_symbols"
+
+DEFAULT_SCAN_INTERVAL: Final = timedelta(hours=6)
+MINIMUM_SCAN_INTERVAL: Final = timedelta(seconds=30)
+
+CURRENCY_CODES: Final = {
+    "bdt": "৳",
+    "brl": "R$",
+    "btc": "₿",
+    "cny": "¥",
+    "eth": "Ξ",
+    "eur": "€",
+    "gbp": "£",
+    "ils": "₪",
+    "inr": "₹",
+    "jpy": "¥",
+    "krw": "₩",
+    "kzt": "лв",
+    "ngn": "₦",
+    "php": "₱",
+    "rial": "﷼",
+    "rub": "₽",
+    "sign": "",
+    "try": "₺",
+    "twd": "$",
+    "usd": "$",
+}
